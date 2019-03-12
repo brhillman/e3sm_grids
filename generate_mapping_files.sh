931923253c2c5e016058ba458a7728ae8a3185dd
Benjamin Hillman <bhillma@sandia.gov>
2019-03-12 12:56:39 -0700

    Add maps between all components

--- generate_mapping_files.sh
+++ generate_mapping_files.sh
@@ -11,7 +11,8 @@ fi
 
 # Load a common conda environment for E3SM pre and post processing tools
 #source /global/project/projectdirs/acme/software/anaconda_envs/load_latest_e3sm_unified.sh
-#conda activate e3sm-unified
+module load python
+source activate e3sm-unified
 
 # Append path to include TempestRemap path
 tempest_path=${PWD}/tempestremap/bin
@@ -20,11 +21,31 @@ PATH=${tempest_path}:${PATH}
 # Need to override hard-coded paths in NCO scripts
 export NCO_PATH_OVERRIDE='No'
 
-# Generate mapping files between atmosphere and ocean grids
+# Generate mapping files between all grids
 datestring=`date +'%y%m%d'`
-mapping_root=${output_root}/tempest_maps
+mapping_root=${output_root}/mapping_files
 mkdir -p ${mapping_root} && cd ${mapping_root}
-ncremap -P mwf \
-    -s ${ocn_scrip_file} -g ${atm_mesh_file} \
-    --nm_src=${ocn_grid_name} --nm_dst=${atm_grid_name} \
-    --dt_sng=${datestring}
+
+if [ "${ocn_grid_name}" != "${atm_grid_name}" ]; then
+    echo "Map ocean to atmosphere..."
+    ncremap -P mwf \
+        -s ${ocn_scrip_file} -g ${atm_mesh_file} \
+        --nm_src=${ocn_grid_name} --nm_dst=${atm_grid_name} \
+        --dt_sng=${datestring}
+fi
+
+if [ "${atm_grid_name}" != "${lnd_grid_name}" ]; then
+    echo "Map land to atmosphere..."
+    ncremap -P mwf \
+        -s ${lnd_scrip_file} -g ${atm_mesh_file} \
+        --nm_src=${lnd_grid_name} --nm_dst=${atm_grid_name} \
+        --dt_sng=${datestring}
+fi
+
+if [ "${ocn_grid_name}" != "${lnd_grid_name}" ] && [ "${atm_grid_name}" != "${lnd_grid_name}" ]; then
+    echo "Map ocean to land..."
+    ncremap -P mwf \
+        -s ${ocn_scrip_file} -g ${lnd_scrip_file} \
+        --nm_src=${ocn_grid_name} --nm_dst=${lnd_grid_name} \
+        --dt_sng=${datestring}
+fi
