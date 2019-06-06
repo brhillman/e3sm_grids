299f7d3e23f035f8a2beeaa920314fc257a652cf
Benjamin Hillman <bhillma@sandia.gov>
2019-06-06 10:31:22 -0700

    Add optional args to mapping files

--- generate_mapping_files.sh
+++ generate_mapping_files.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
 # Parse command line arguments
-if [ $# -eq 1 ]; then
+if [ $# -ge 1 ]; then
     configuration_file=$1
     source ${configuration_file}
+    shift
 else
     echo "usage: `basename $0` <configuration_file>"
     exit 1
 fi
+
+# Parse optional arguments
+method="tempest"
+for arg in "$@"; do
+    case $arg in
+        --method=*)
+            method="${arg#*=}"
+            shift
+            ;;
+        *)
+            echo "Error parsing ${arg}. See usage."
+            exit 1
+            ;;
+    esac
+done
+
 
 # Load a common conda environment for E3SM pre and post processing tools
 #source /global/project/projectdirs/acme/software/anaconda_envs/load_latest_e3sm_unified.sh
@@ -19,12 +36,11 @@ tempest_path=${PWD}/tempestremap/bin
 PATH=${tempest_path}:${PATH}
 
 # Need to override hard-coded paths in NCO scripts
-export NCO_PATH_OVERRIDE='No'
+#export NCO_PATH_OVERRIDE='No'
 
 # Generate mapping files between all grids
 datestring=`date +'%y%m%d'`
 
-method="tempest"
 if [ "${method}" == "esmf" ]; then
     atm_grid_file=${atm_scrip_file}
 else
@@ -37,28 +53,29 @@ mkdir -p ${mapping_root} && cd ${mapping_root}
 # Maps between atmosphere and ocean
 if [ "${ocn_grid_name}" != "${atm_grid_name}" ]; then
     echo "Map ocean to atmosphere..."
+    cd ${mapping_root}
     ncremap -P mwf \
         -s ${ocn_scrip_file} -g ${atm_grid_file} \
         --nm_src=${ocn_grid_name} --nm_dst=${atm_grid_name} \
         --dt_sng=${datestring}
 fi
 
-# Maps between atmosphere and land (for tri-grid)
-if [ "${atm_grid_name}" != "${lnd_grid_name}" ]; then
-    echo "Map land to atmosphere..."
-    cd ${mapping_root}
-    ncremap -P mwf \
-        -s ${lnd_scrip_file} -g ${atm_grid_file} \
-        --nm_src=${lnd_grid_name} --nm_dst=${atm_grid_name} \
-        --dt_sng=${datestring}
-fi
-
-# Maps between ocean and land (for domain files if running tri-grid)
-if [ "${atm_grid_name}" != "${lnd_grid_name}" ]; then
-    echo "Map ocean to land..."
-    cd ${mapping_root}
-    ncremap -P mwf \
-        -s ${ocn_scrip_file} -g ${lnd_scrip_file} \
-        --nm_src=${ocn_grid_name} --nm_dst=${lnd_grid_name} \
-        --dt_sng=${datestring} 
-fi
+## Maps between atmosphere and land (for tri-grid)
+#if [ "${atm_grid_name}" != "${lnd_grid_name}" ]; then
+#    echo "Map land to atmosphere..."
+#    cd ${mapping_root}
+#    ncremap -P mwf \
+#        -s ${lnd_scrip_file} -g ${atm_grid_file} \
+#        --nm_src=${lnd_grid_name} --nm_dst=${atm_grid_name} \
+#        --dt_sng=${datestring}
+#fi
+#
+## Maps between ocean and land (for domain files if running tri-grid)
+#if [ "${atm_grid_name}" != "${lnd_grid_name}" ]; then
+#    echo "Map ocean to land..."
+#    cd ${mapping_root}
+#    ncremap -P mwf \
+#        -s ${ocn_scrip_file} -g ${lnd_scrip_file} \
+#        --nm_src=${ocn_grid_name} --nm_dst=${lnd_grid_name} \
+#        --dt_sng=${datestring} 
+#fi
