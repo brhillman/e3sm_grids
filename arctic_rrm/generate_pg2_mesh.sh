ed14b384858720f52452f46be59b133103462c29
Benjamin R. Hillman <bhillma@sandia.gov>
2021-04-29 10:34:19 -0600

    Use config.sh for generate_pg2_mesh.sh

--- arctic_rrm/generate_pg2_mesh.sh
+++ arctic_rrm/generate_pg2_mesh.sh
@@ -2,9 +2,7 @@
 # See:
 # https://acme-climate.atlassian.net/wiki/spaces/ED/pages/1043235115/Special+Considerations+for+FV+Physics+Grids
 source config.sh
-output_root=/sems-data-store/ACME/bhillma/grids/${atm_grid_name}
-tempest_root=${HOME}/codes/e3sm_grids/tempestremap/build
-${tempest_root}/GenerateVolumetricMesh \
+${tempest_root}/bin/GenerateVolumetricMesh \
     --in ${output_root}/${atm_grid_name}.g \
     --out ${output_root}/${atm_grid_name}pg2.g \
     --np 2 --uniform
