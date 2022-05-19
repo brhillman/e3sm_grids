ae51a981fe76f33cd5cb95d4b5b721c78263e9bf
Benjamin R. Hillman <bhillma@sandia.gov>
2022-05-18 22:18:23 -0600

    Add generate_atmsrf.sh for ne256np4

--- ne256np4/generate_atmsrf.sh
+++ ne256np4/generate_atmsrf.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
+
 set -e
 
 # Load config
 source config.sh
 
+# Set full name for physics grid
+physics_grid=${atm_grid_name}
+
 # Build mkatmsrffile
 cd ${e3sm_root}/components/eam/tools/mkatmsrffile || exit 1
 eval $(${e3sm_root}/cime/CIME/Tools/get_case_env)
-NETCDF_ROOT=${NETCDF_DIR} FC=ifort make
+${e3sm_root}/cime/CIME/scripts/configure --macros-format=Makefile || exit 1
+source .env_mach_specific.sh || exit 1
+NETCDF_ROOT=${NETCDFROOT} FC=gfortran make
 
 # Create a mapping file
-map_file="map_1x1d_to_${atm_grid_name}_mono.nc"
-if [ ! -e ${map_file} ]; then
+mkdir -p ${output_root}/atmsrf && cd ${output_root}/atmsrf
+if [ ! -e ${output_root}/atmsrf/map_1x1_to_${physics_grid}_mono.nc ]; then
     ncremap -a tempest \
         --src_grd=${mapping_root}/grids/1x1d.nc --dst_grd=${atm_grid_file} \
-        -m ${map_file} \
-        -W '--in_type fv --in_np 1 --out_type cgll --out_np 4 --mono --correct_areas --out_format Classic'
+        -m ${output_root}/map_1x1_to_${physics_grid}_mono.nc \
+        -W '--in_type fv --in_np 1 --out_type cgll --out_np 4 --out_format Offset64Bits'
+    #ncremap -a bilin \
+    #    --src_grd=${mapping_root}/grids/1x1d.nc --dst_grd=${atm_scrip_file} \
+    #    -m ${output_root}/map_1x1_to_${physics_grid}_bilin
 fi
 
 echo ${mapping_root}/grids/1x1d.nc
 echo ${inputdata_root}/atm/cam/chem/trop_mozart/dvel/regrid_vegetation.nc
 echo ${inputdata_root}/atm/cam/chem/trop_mozart/dvel/clim_soilw.nc
-echo ${map_file}
+echo ${output_root}/map_1x1_to_${physics_grid}_mono.nc
 
 # Edit namelist
 date=`date +%Y%m%d`
@@ -31,8 +40,8 @@ srfFileName = '${mapping_root}/grids/1x1d.nc'
 landFileName = '${inputdata_root}/atm/cam/chem/trop_mozart/dvel/regrid_vegetation.nc'
 soilwFileName = '${inputdata_root}/atm/cam/chem/trop_mozart/dvel/clim_soilw.nc'
 atmFileName = '${atm_scrip_file}'
-srf2atmFmapname = '${map_file}'
-outputFileName = '${output_root}/atmsrf_${atm_grid_name}_${date}_n4.nc'
+srf2atmFmapname = '${output_root}/map_1x1_to_${physics_grid}_mono.nc'
+outputFileName = '${output_root}/atmsrf_${physics_grid}_${date}_n4.nc'
 /
 EOF
 
@@ -40,9 +49,9 @@ EOF
 mkatmsrffile=${e3sm_root}/components/eam/tools/mkatmsrffile/mkatmsrffile
 ${mkatmsrffile}
 
-# Convert the file to cdf5 format:
-if [ -e atmsrf_${atm_grid_name}_${date}_n4.nc ]; then
-    ncks -5 atmsrf_${atm_grid_name}_${date}_n4.nc atmsrf_${atm_grid_name}_${date}.nc
+# Convert the file format:
+if [ -e atmsrf_${physics_grid}_${date}_n4.nc ]; then
+    ncks -5 atmsrf_${physics_grid}_${date}_n4.nc atmsrf_${physics_grid}_${date}.nc
 fi
 
 # Exit gracefully
