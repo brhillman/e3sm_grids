9d3d3090f4044365230f056886911e33f3e4fc39
Benjamin R. Hillman <bhillma@sandia.gov>
2022-05-20 14:06:02 -0600

    Fix generate_atmsrf.sh

--- ne256np4/generate_atmsrf.sh
+++ ne256np4/generate_atmsrf.sh
@@ -17,41 +17,39 @@ NETCDF_ROOT=${NETCDFROOT} FC=gfortran make
 
 # Create a mapping file
 mkdir -p ${output_root}/atmsrf && cd ${output_root}/atmsrf
-if [ ! -e ${output_root}/atmsrf/map_1x1_to_${physics_grid}_mono.nc ]; then
+map_file=${output_root}/atmsrf/map_1x1_to_${physics_grid}_mono.nc
+if [ ! -f ${map_file} ]; then
     ncremap -a tempest \
         --src_grd=${mapping_root}/grids/1x1d.nc --dst_grd=${atm_grid_file} \
-        -m ${output_root}/map_1x1_to_${physics_grid}_mono.nc \
+        -m ${map_file} \
         -W '--in_type fv --in_np 1 --out_type cgll --out_np 4 --out_format Offset64Bits'
     #ncremap -a bilin \
     #    --src_grd=${mapping_root}/grids/1x1d.nc --dst_grd=${atm_scrip_file} \
     #    -m ${output_root}/map_1x1_to_${physics_grid}_bilin
 fi
 
-echo ${mapping_root}/grids/1x1d.nc
-echo ${inputdata_root}/atm/cam/chem/trop_mozart/dvel/regrid_vegetation.nc
-echo ${inputdata_root}/atm/cam/chem/trop_mozart/dvel/clim_soilw.nc
-echo ${output_root}/map_1x1_to_${physics_grid}_mono.nc
-
 # Edit namelist
 date=`date +%Y%m%d`
+atmsrf_file=${output_root}/atmsrf/atmsrf_${physics_grid}_${date}.nc
 cat <<EOF > nml_atmsrf
 &input
 srfFileName = '${mapping_root}/grids/1x1d.nc'
 landFileName = '${inputdata_root}/atm/cam/chem/trop_mozart/dvel/regrid_vegetation.nc'
 soilwFileName = '${inputdata_root}/atm/cam/chem/trop_mozart/dvel/clim_soilw.nc'
 atmFileName = '${atm_scrip_file}'
-srf2atmFmapname = '${output_root}/map_1x1_to_${physics_grid}_mono.nc'
-outputFileName = '${output_root}/atmsrf_${physics_grid}_${date}_n4.nc'
+srf2atmFmapname = '${map_file}'
+outputFileName = '${atmsrf_file}'
 /
 EOF
 
-# Run the tool
-mkatmsrffile=${e3sm_root}/components/eam/tools/mkatmsrffile/mkatmsrffile
-${mkatmsrffile}
 
-# Convert the file format:
-if [ -e atmsrf_${physics_grid}_${date}_n4.nc ]; then
-    ncks -5 atmsrf_${physics_grid}_${date}_n4.nc atmsrf_${physics_grid}_${date}.nc
+if [ ! -f ${atmsrf_file} ]; then
+    # Run the tool
+    mkatmsrffile=${e3sm_root}/components/eam/tools/mkatmsrffile/mkatmsrffile
+    ${mkatmsrffile}
+
+    # Convert the file format:
+    ncks -5 -O ${atmsrf_file} ${atmsrf_file}
 fi
 
 # Exit gracefully
