9050208316e8416d20f00c3a83df9c856af0ff2f
Benjamin Hillman <bhillma@sandia.gov>
2023-03-29 11:02:00 -0700

    Add script to generate gll scrip file using homme_tool

---
+++ generate_gll_scrip.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -eq 1 ]; then
+    source $1
+else
+    echo "usage: $0 <config file>"
+    exit 1
+fi
+
+# Build homme_tool
+${e3sm_root}/cime/CIME/scripts/configure && source .env_mach_specific.sh
+eval $( ${e3sm_root}/cime/CIME/Tools/get_case_env ) #&& source .env_mach_specific.sh
+
+ntasks=4
+homme_tool_root=${e3sm_root}/components/homme/test/tool
+mkdir -p ${output_root}/topo
+cd ${output_root}/topo
+#cd ${homme_tool_root}
+cmake \
+    -C ${homme_tool_root}/../../cmake/machineFiles/perlmutter-nocuda-gnu.cmake \
+    -DBUILD_HOMME_WITHOUT_PIOLIBRARY=OFF \
+    -DPREQX_PLEV=26 \
+    -DUSE_NUM_PROCS=${ntasks} \
+    ${homme_tool_root}/../../
+make -j4 homme_tool
+
+# Run homme_tool
+cat > input.nl <<EOF
+&ctl_nl
+ne = ${atm_resolution}
+mesh_file = "none"
+/
+&vert_nl
+/
+&analysis_nl
+tool = 'grid_template_tool'
+output_dir = "./"
+output_timeunits=1
+output_frequency=1
+output_varnames1='area','corners','cv_lat','cv_lon'
+output_type='netcdf'
+!output_type='netcdf4p'  ! needed for ne1024
+io_stride = 16
+/
+EOF
+srun --nodes=1 --ntasks=${ntasks} ./src/tool/homme_tool < input.nl
+
+# make the 'scrip' file for target GLL grid
+ncks -O -v lat,lon,area,cv_lat,cv_lon ne${atm_resolution}np4_tmp1.nc ne${atm_resolution}np4_tmp.nc
+ncl ${e3sm_root}/components/homme/test/tool/ncl/HOMME2SCRIP.ncl  name=\"ne${atm_resolution}np4\"  ne=${atm_resolution}  np=4
