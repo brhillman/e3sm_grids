478da289826b7ce139581ac0b9e38ccd054f3c72
Benjamin Hillman <bhillma@sandia.gov>
2023-04-04 08:47:54 -0700

    Add config file for perlmutter

---
+++ configs/config.pm-cpu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+tools_root=${PSCRATCH}/e3sm_grids/tools
+e3sm_root=${HOME}/codes/e3sm/branches/master
+inputdata_root=/global/cfs/cdirs/e3sm/inputdata
+mapping_root=/global/cfs/cdirs/e3sm/mapping
+output_root=/global/cfs/cdirs/e3sm/bhillma/grids
