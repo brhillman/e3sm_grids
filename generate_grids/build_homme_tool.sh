2607756747a13ba0984ff4d365a2e8abb9551714
Benjamin Hillman <bhillma@sandia.gov>
2023-04-04 07:44:39 -0700

    Use command line args for homme scripts

--- generate_grids/build_homme_tool.sh
+++ generate_grids/build_homme_tool.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]; then
+function usage () {
+    echo "usage: $0 <machine config> [--ntasks N]"
+}
+
+# Check command line arguments
+if [ $# -ge 1 ]; then
     source $1
 else
-    echo "usage: $0 <config>"
+    usage
     exit 1
 fi
 
-tools_root=${PWD}/tools
+# Parse optional arguments
+ntasks=4  # Default number of mpi ranks to build tool for
+while [ "$2" != "" ]; do
+    case $2 in
+        -n | --ntasks )
+            shift
+            ntasks=$2
+            ;;
+        *)
+            usage
+            exit 1
+    esac
+    shift
+done
 
 # Build homme_tool
+echo "Building homme_tool for ${ntasks} ranks..."
 eval $( ${e3sm_root}/cime/CIME/Tools/get_case_env ) #&& source .env_mach_specific.sh
-ntasks=4
 homme_tool_root=${e3sm_root}/components/homme/test/tool
 mkdir -p ${tools_root}/homme_tool
 cd ${tools_root}/homme_tool
--- generate_grids/generate_gll_scrip.sh
+++ generate_grids/generate_gll_scrip.sh
@@ -2,20 +2,36 @@
 
 set -e
 
-if [ $# -eq 1 ]; then
+function usage () {
+    echo "usage: $0 <machine config file> <resolution> [options]"
+}
+
+# Get positional arguments
+if [ $# -ge 2 ]; then
     source $1
+    atm_resolution=$2
 else
-    echo "usage: $0 <config file>"
+    usage
     exit 1
 fi
 
-tools_root=${PWD}/tools
-
-# Build homme_tool
-ntasks=4
-
-mkdir -p ${output_root}/grids
-cd ${output_root}/grids
+# Get optional arguments
+mpirun=
+while [ "$3" != "" ]; do
+    case $3 in 
+        -m | --mpirun )
+            shift
+            mpirun=$3
+            ;;
+        *)
+            usage
+            exit 1
+    esac
+    shift
+done
+atm_grid_name=ne${atm_resolution}
+mkdir -p ${output_root}/${atm_grid_name}/grids
+cd ${output_root}/${atm_grid_name}/grids
 
 # Run homme_tool
 eval $( ${e3sm_root}/cime/CIME/Tools/get_case_env ) #&& source .env_mach_specific.sh
@@ -37,7 +53,7 @@ output_type='netcdf'
 io_stride = 16
 /
 EOF
-srun --nodes=1 --ntasks=${ntasks} --constraint=cpu --time=00:10:00 ${tools_root}/homme_tool/src/tool/homme_tool < input.nl
+${mpirun} ${tools_root}/homme_tool/src/tool/homme_tool < input.nl
 
 # make the 'scrip' file for target GLL grid
 ncks -O -v lat,lon,area,cv_lat,cv_lon ne${atm_resolution}np4_tmp1.nc ne${atm_resolution}np4_tmp.nc
